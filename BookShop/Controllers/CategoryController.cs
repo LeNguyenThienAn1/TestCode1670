40987016cf1d5f33749d5b6ea48690e0956411c4
Pham Thanh Son <sonpt28@fpt.edu.vn>
2023-11-25 13:57:47 +0700

    Notification and Edit Action

--- BookShop/Controllers/CategoryController.cs
+++ BookShop/Controllers/CategoryController.cs
@@ -26,15 +26,44 @@ namespace BookShop.Controllers
 		}
 		[HttpPost]
 		public IActionResult Create(Category category){ 
-		
+
+			if (category.Name == category.Description)
+			{
+				ModelState.AddModelError("Description", "Name can not be equal to Description");
+			}
 			if (ModelState.IsValid)
 			{
 				_dbContext.Categories.Add(category);
 				_dbContext.SaveChanges();
 				TempData["success"] = "Category Created successfully";
-			}
-			return RedirectToAction("Index");
-
+                return RedirectToAction("Index");
+            }
+			return View();
 		}
+		public IActionResult Edit(int? id)
+		{
+			if (id ==null  || id == 0) 
+			{
+				return NotFound();
+			}
+			Category? category = _dbContext.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category Updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 	}
 }
