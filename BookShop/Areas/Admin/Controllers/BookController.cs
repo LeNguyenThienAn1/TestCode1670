9c8198f2371b97ff433c0f5cd9ffd202148e07e9
Pham Thanh Son <sonpt28@fpt.edu.vn>
2023-12-02 14:08:10 +0700

    Add Area

--- BookShop/Areas/Admin/Controllers/BookController.cs
+++ BookShop/Areas/Admin/Controllers/BookController.cs
@@ -5,13 +5,14 @@ using BookShop.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace BookShop.Controllers
+namespace BookShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class BookController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _webHostEnvironment;
-        public BookController(IUnitOfWork unitOfWork,IWebHostEnvironment webHostEnvironment)
+        public BookController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
             _webHostEnvironment = webHostEnvironment;
@@ -21,44 +22,45 @@ namespace BookShop.Controllers
             List<Book> books = _unitOfWork.BookRepository.GetAll("Category").ToList();
             return View(books);
         }
-		public IActionResult CreateUpdate(int? id)
-		{
-			BookVM bookVM = new BookVM()
+        public IActionResult CreateUpdate(int? id)
+        {
+            BookVM bookVM = new BookVM()
             {
-                Categories = _unitOfWork.CategoryRepository.GetAll().Select(c=>new SelectListItem 
+                Categories = _unitOfWork.CategoryRepository.GetAll().Select(c => new SelectListItem
                 {
                     Text = c.Name,
                     Value = c.Id.ToString(),
                 }),
-                Book = new Book() 
+                Book = new Book()
             };
-			if (id == null||id==0) {
+            if (id == null || id == 0)
+            {
                 //Create
                 return View(bookVM);
             }
-			else
-			{
-				//Update
-				bookVM.Book = _unitOfWork.BookRepository.Get(b=>b.Id==id);
-				return View(bookVM);
-			}
-			
-		}
-		[HttpPost]
-		public IActionResult CreateUpdate(BookVM bookVM,IFormFile? file)
-		{
-            
-			if (ModelState.IsValid)
-			{
+            else
+            {
+                //Update
+                bookVM.Book = _unitOfWork.BookRepository.Get(b => b.Id == id);
+                return View(bookVM);
+            }
+
+        }
+        [HttpPost]
+        public IActionResult CreateUpdate(BookVM bookVM, IFormFile? file)
+        {
+
+            if (ModelState.IsValid)
+            {
                 string wwwRootPath = _webHostEnvironment.WebRootPath;
                 if (file != null)
                 {
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string bookPath = Path.Combine(wwwRootPath, @"images\books");
 
-                    if (!String.IsNullOrEmpty(bookVM.Book.ImageUrl))
+                    if (!string.IsNullOrEmpty(bookVM.Book.ImageUrl))
                     {
-                        var oldImagePath = Path.Combine(wwwRootPath,bookVM.Book.ImageUrl.TrimStart('\\'));
+                        var oldImagePath = Path.Combine(wwwRootPath, bookVM.Book.ImageUrl.TrimStart('\\'));
                         if (System.IO.File.Exists(oldImagePath))
                         {
                             System.IO.File.Delete(oldImagePath);
@@ -68,21 +70,21 @@ namespace BookShop.Controllers
                     {
                         file.CopyTo(fileStream);
                     }
-                    bookVM.Book.ImageUrl = @"\images\books\" +fileName;
+                    bookVM.Book.ImageUrl = @"\images\books\" + fileName;
                 }
-				if (bookVM.Book.Id == 0)
-				{
+                if (bookVM.Book.Id == 0)
+                {
                     _unitOfWork.BookRepository.Add(bookVM.Book);
                     TempData["success"] = "Book Created successfully";
                 }
-				else
-				{
+                else
+                {
                     _unitOfWork.BookRepository.Update(bookVM.Book);
                     TempData["success"] = "Book Updated successfully";
                 }
-				
-				_unitOfWork.Save();
-				return RedirectToAction("Index");
+
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
             }
             else
             {
@@ -97,8 +99,8 @@ namespace BookShop.Controllers
                 };
                 return View(bookVMNew);
             }
-			
-		}
+
+        }
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
